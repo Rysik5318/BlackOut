aa308fd7304b277c66593e2d3463d35bf32ff03f
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2022-11-27 02:20:59 +0400

    Add files via upload

--- Config.cs
+++ Config.cs
@@ -1,11 +1,6 @@
 ﻿using Exiled.API.Enums;
 using Exiled.API.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlackOut
 {
--- Plugin.cs
+++ Plugin.cs
@@ -1,70 +1,34 @@
-﻿using Exiled.API.Enums;
-using Exiled.API.Features;
-using Exiled.Events.EventArgs;
+﻿using Exiled.API.Features;
+using Server = Exiled.Events.Handlers.Server;
 using MEC;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlackOut
 {
-    class Plugin : Plugin<Config>
+    public class Plugin : Plugin<Config>
     {
+        internal Random Rand;
         public override string Prefix => "BlackOut";
         public override string Name => "BlackOut";
         public override string Author => "Rysik5318";
-
-        public static Plugin plugin;
-        public override Version Version { get; } = new Version(1, 1, 7);
-
-        bool BlackoutOFF = false;
-
-        bool RoundStart = false;
+        public override Version Version { get; } = new Version(1, 1, 8);
+        private EventHandlers Handlers;
 
         public override void OnEnabled()
         {
-            plugin = this;
+            Handlers = new EventHandlers(this);
+            Rand = new Random();
+            Server.RoundStarted += Handlers.OnRoundStarted;
             base.OnEnabled();
-            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStart;
         }
 
         public override void OnDisabled()
         {
-            plugin = null;
-            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStart;
+            Timing.KillCoroutines(Handlers.BlackoutCoroutine);
+            Server.RoundStarted -= Handlers.OnRoundStarted;
+            Rand = null;
+            Handlers = null;
             base.OnDisabled();
-        }
-
-        private void OnRoundStart()
-        {
-            Random rnd = new Random();
-            BlackoutOFF = false;
-            RoundStart = true;
-            Timing.CallDelayed(rnd.Next(Config.MinBlackoutStartTime, Config.MaxBlackoutStartTime), () => Blackout());
-        }
-        private void Blackout()
-        {
-            if (RoundStart)
-            {
-                if (!BlackoutOFF)
-                {
-                        IEnumerable<ZoneType> zoneTypes = new List<ZoneType>() { ZoneType.Entrance, ZoneType.HeavyContainment, ZoneType.LightContainment };
-                        Random rnd = new Random();
-                        float time = rnd.Next(Config.MinBlackoutTime, Config.MaxBlackoutTime);
-                        Map.TurnOffAllLights(time, ZoneType.Entrance);
-                        Map.TurnOffAllLights(time, ZoneType.HeavyContainment);
-                        Map.TurnOffAllLights(time, ZoneType.LightContainment);
-                        if (Config.offCassie)
-                        {
-                            Timing.CallDelayed(3f, () => Cassie.Message(Config.CassieLight, false, true, Config.Subtitles));
-                        }
-                        else if (!Config.offCassie);
-                        Timing.CallDelayed(rnd.Next(Config.MinBlackoutStartTime, Config.MaxBlackoutStartTime), () => Blackout());
-                        BlackoutOFF = true;
-                }
-            }
         }
     }
 }
